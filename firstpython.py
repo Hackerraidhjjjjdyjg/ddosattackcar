a3e4f8013a1d6d8d19f59ccc41d06bd8ed028b21
Hackerraidhjjjjdyjg <128967744+Hackerraidhjjjjdyjg@users.noreply.github.com>
2025-12-15 17:48:56 +0100

    using the plot function
    Fix syntax errors and improve comments in firstpython.py

---
+++ firstpython.py
@@ -0,0 +1,6 @@
+# Define the cars vectors wit 5 values
+cars <-c(1,4,6,5,10)
+#grafh the cars vectors with defaults
+plot (cars, type= "0"
+      " create a title 
+      title (main= " carvs  index ")
